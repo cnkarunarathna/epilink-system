f6bec8ca2c06dc82fb61bd8d95448ef3ccc5a78b
cnkarunarathna <nuwanthakonline@gmail.com>
2026-04-03 15:36:12 +0530

    implement route optimization service with OSRM integration and DTOs

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { GeocodingService } from './geocoding.service';
+import { RouteService } from './route.service';
 import { Task } from './entities/task.entity';
 import { Evidence } from './entities/evidence.entity';
 import { User } from '../entities/user.entity';
@@ -11,7 +12,7 @@ import { StorageModule } from '../storage/storage.module';
 @Module({
   imports: [TypeOrmModule.forFeature([Task, Evidence, User]), StorageModule],
   controllers: [TasksController],
-  providers: [TasksService, GeocodingService],
-  exports: [TasksService, GeocodingService],
+  providers: [TasksService, GeocodingService, RouteService],
+  exports: [TasksService, GeocodingService, RouteService],
 })
 export class TasksModule {}
